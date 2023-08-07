7b7ad3d17b5391204604a2a9e0045b74d74b403d
kaka1804 <kaue.barbosa@aluno.ifsp.edu.br>
2023-08-07 17:55:48 -0300

    second comit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const api = express()
+
+api.get('/', (req, res) => (
+    res.json({message: "Bem-vindo a nossa API"})
+))
+
+api.listen(3000, () => {
+    console.log ('servidor rodando na porta 3000. http://localhost:3000')
+})
